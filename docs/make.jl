2c1660b3489d6657f0df539b2b6a78f9560522b4
cgarling <chris.t.garling@gmail.com>
2025-04-18 11:16:47 -0400

    edit package setup

--- docs/make.jl
+++ docs/make.jl
@@ -3,11 +3,14 @@ using Documenter
 
 DocMeta.setdocmeta!(StellarTracks, :DocTestSetup, :(using StellarTracks); recursive=true)
 
+# The `format` below makes it so that urls are set to "pretty" if you are pushing them to a hosting service, and basic if you are just using them locally to make browsing easier.
+
 makedocs(;
     modules=[StellarTracks],
-    authors="cgarling <chris.t.garling@gmail.com> and contributors",
+    authors="Chris Garling",
     sitename="StellarTracks.jl",
     format=Documenter.HTML(;
+        prettyurls = get(ENV, "CI", nothing) == "true",
         canonical="https://cgarling.github.io/StellarTracks.jl",
         edit_link="main",
         assets=String[],
@@ -22,5 +25,7 @@ makedocs(;
 
 deploydocs(;
     repo="github.com/cgarling/StellarTracks.jl",
+    versions = ["stable" => "v^", "v#.#"],
     devbranch="main",
+    push_preview=true
 )
