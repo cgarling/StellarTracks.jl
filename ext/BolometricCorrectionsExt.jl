7790e8c9dcb8769f449ea5a69f3d4e29ddcce4a4
cgarling <chris.t.garling@gmail.com>
2025-04-20 17:56:23 -0400

    Switch to table interface `isochrone` BC extension
    Good to have the theoretical isochrone quantities and the BC magnitudes

--- ext/BolometricCorrectionsExt.jl
+++ ext/BolometricCorrectionsExt.jl
@@ -2,7 +2,9 @@ module BolometricCorrectionsExt
 
 using StellarTracks: AbstractTrackSet, PARSECLibrary
 import StellarTracks: isochrone
+import StellarTracks as ST
 using BolometricCorrections: AbstractBCTable, MISTBCGrid, filternames
+import BolometricCorrections as BC
 
 using TypedTables: Table
 import Tables
@@ -46,14 +48,13 @@ function _parse_Mbol(iso)
 end
 
 """
-    isochrone([::Type{TypedTables.Table},]
-              ts::StellarTracks.AbstractTrackSet,
+    isochrone(ts::StellarTracks.AbstractTrackSet,
               bc::BolometricCorrections.AbstractBCTable,
               logAge::Number)
-Returns an isochrone as a matrix with dimensions `(nstars, nfilters)` calculated using the stellar evolutionary tracks contained in `ts` with bolometric corrections interpolated from the provided table `bc` at the logarithmic age `logAge`. The three-argument version that returns a `Table` has a roughly fixed runtime overhead cost of 3--5 μs to perform the type conversion.
+Returns an isochrone as a `TypedTables.Table` calculated using the stellar evolutionary tracks contained in `ts` with bolometric corrections interpolated from the provided table `bc` at the logarithmic age `logAge`. Column names can be retrieved with `TypedTables.columnnames`. The result can be converted to a matrix with `Tables.matrix`.
 
 # Examples
-Default call signature returning a matrix.
+
 ```julia
 using StellarTracks, BolometricCorrections
 p = PARSECLibrary() # Load PARSEC library of stellar models
@@ -62,18 +63,7 @@ isochrone(p.ts[1], m(1e-4, 0.0), 10.0)
 ```
 
 ```
-1323×29 Matrix{Float64}
-...
-```
-
-Call signature returning a `TypedTables.Table`.
-```julia
-using TypedTables: Table
-isochrone(Table, p.ts[1], m(1e-4, 0.0), 10.0)
-```
-
-```
-Table with 29 columns and 1323 rows:
+Table with 35 columns and 1323 rows:
 ...
 ```
 """
@@ -83,16 +73,13 @@ function isochrone(ts::AbstractTrackSet, bc::AbstractBCTable, logAge::Number)
     iso_logg = _parse_logg(iso)
     iso_Mbol = _parse_Mbol(iso)
     BCs = permutedims(bc(iso_teff, iso_logg))
-    return iso_Mbol .- BCs
-end
-function isochrone(::Type{Table}, ts::AbstractTrackSet, bc::AbstractBCTable, logAge::Number)
-    filters = filternames(bc)
-    # Mostly fixed ~3--5 μs runtime cost
-    return Table(Tables.table(isochrone(ts, bc, logAge); header=filters))
+    # Concatenate theoretical quantities with magnitudes
+    return Table(Table(iso),
+                 Table(Tables.table(iso_Mbol .- BCs; header=filternames(bc))))
 end
 
 # Not sure how to handle the fact that AbstractTrackLibrary and AbstractBCGrid can
-# have different dependent variables (Z, A_V, α-abundance, etc.). For now
+# have different dependent variables (Z, Av, α-abundance, etc.). For now
 # going to set up some specific call signatures that be relatively simple to extend.
 function isochrone(tl::PARSECLibrary, bc::AbstractBCTable, logAge::Number, Z::Number)
     iso = isochrone(tl, logAge, Z)
@@ -100,18 +87,55 @@ function isochrone(tl::PARSECLibrary, bc::AbstractBCTable, logAge::Number, Z::Nu
     iso_logg = _parse_logg(iso)
     iso_Mbol = _parse_Mbol(iso)
     BCs = permutedims(bc(iso_teff, iso_logg))
-    return iso_Mbol .- BCs
+    # Concatenate theoretical quantities with magnitudes
+    return Table(Table(iso),
+                 Table(Tables.table(iso_Mbol .- BCs; header=filternames(bc))))
 end
-function isochrone(::Type{Table}, tl::PARSECLibrary, bc::AbstractBCTable,
-                   logAge::Number, Z::Number)
-    filters = filternames(bc)
-    # Mostly fixed ~3--5 μs runtime cost
-    return Table(Tables.table(isochrone(tl, bc, logAge, Z); header=filters))
+function isochrone(tl::PARSECLibrary, bcg::MISTBCGrid, logAge::Number, Z::Number, Av::Number)
+    # MISTBCGrid expects metallicity in [Fe/H] == [M/H], not Z, so convert
+    mh = BC.MIST.MH(BC.MIST.MISTChemistry(), Z)
+    return isochrone(tl, bcg(mh, Av), logAge, Z)
 end
-isochrone(tl::PARSECLibrary, bcg::MISTBCGrid, logAge::Number, Z::Number, A_V::Number) =
-    isochrone(tl, bcg(Z, A_V), logAge, Z)
-isochrone(::Type{Table}, tl::PARSECLibrary, bcg::MISTBCGrid,
-          logAge::Number, Z::Number, A_V::Number) =
-    isochrone(Table, tl, bcg(Z, A_V), logAge, Z)
+# This is most useful for constructing large isochrone tables that can then be written out.
+# For large grids (3403 isochrones) selecting out a single isochrone takes longer than
+# running a new one (9.381 ms vs 1.2 ms). When constructing isochrones in order to make
+# partial CMD templates, it is better just to sample them one-by-one in the threaded loop
+# rather than trying to pre-generate them all. 
+function isochrone(tl::PARSECLibrary, bcg::MISTBCGrid, logAge::AbstractArray{<:Number}, Z::AbstractArray{<:Number}, Av::Number)
+    result = []
+    rlock = ReentrantLock()
+    # This implementation returns a single Table, with Z and logAge rows 
+    Threads.@threads for Zval in Z
+        mh = BC.MIST.MH(BC.MIST.MISTChemistry(), Zval)
+        bc = bcg(mh, Av)
+        Threads.@threads for la in logAge
+            iso = isochrone(tl, bc, la, Zval)
+            # Lock and push into result, adding Z and logAge columns
+            @lock rlock push!(result, Table(Table(Z=fill(Zval, length(iso)),
+                                                  logAge=fill(la, length(iso))), iso))
+        end
+    end
+    return vcat(result...)
+    # This implementation returns a vector of Tables,
+    # but you don't necessarily know the order they finished ...
+    # zvec = []
+    # zlock = ReentrantLock()
+    # lavec = []
+    # lalock = ReentrantLock()
+    # Threads.@threads for Zval in Z
+    #     mh = BC.MIST.MH(BC.MIST.MISTChemistry(), Zval)
+    #     bc = bcg(mh, Av)
+    #     Threads.@threads for la in logAge
+    #         iso = isochrone(tl, bc, la, Zval)
+    #         @lock rlock push!(result, iso)
+    #         @lock zlock push!(zvec, Zval)
+    #         @lock lalock push!(lavec, la)
+    #     end
+    # end
+    # # Use zvec and lavec to compute the correct permutation vector to put
+    # # result into the correct order. Specifically, ((Z= 0.01, la=(...)), (Z=0.02, la=(...)), ...)
+    # # we iterate la more quickly than Z.
+    # return result, zvec, lavec
+end
 
 end # module
