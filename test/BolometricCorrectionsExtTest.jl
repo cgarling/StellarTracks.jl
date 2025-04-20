3998c7e2f71a95209f10f792fd9b6b2b7e78d4a5
cgarling <chris.t.garling@gmail.com>
2025-04-20 17:56:23 -0400

    Switch to table interface `isochrone` BC extension
    Good to have the theoretical isochrone quantities and the BC magnitudes

--- test/BolometricCorrectionsExtTest.jl
+++ test/BolometricCorrectionsExtTest.jl
@@ -1,5 +1,7 @@
-using StellarTracks, BolometricCorrections
+using StellarTracks: isochrone, PARSECLibrary, Z
+using BolometricCorrections: MISTBCGrid
 using TypedTables: Table
+using Test
 
 p = PARSECLibrary()
 m = MISTBCGrid("JWST")
@@ -7,22 +9,23 @@ m = MISTBCGrid("JWST")
 # Currently can't interpolate a new TrackSet from a PARSECLibrary
 # instance, so we just take the first TrackSet from p.ts
 iso = isochrone(p.ts[1], m(1e-4, 0.0), 10.0)
-@test iso isa Matrix{Float64}
-iso_table = isochrone(Table, p.ts[1], m(1e-4, 0.0), 10.0)
-@test iso_table isa Table
+@test iso isa Table
 
 # Test passing full stellar track library with single BC table
 iso1 = isochrone(p, m(1e-4, 0.0), 10.0, 1e-4)
-@test iso1 isa Matrix{Float64}
+@test iso1 isa Table
 # Test passing full stellar track library with full BC grid
 iso2 = isochrone(p, m, 10.0, 1e-4, 0.0)
-@test iso2 isa Matrix{Float64}
+@test iso2 isa Table
 # These should both be equivalent
 @test iso1 == iso2
-# Test table interface
-t1 = isochrone(Table, p, m(1e-4, 0.0), 10.0, 1e-4)
-@test t1 isa Table
-t2 = isochrone(Table, p, m, 10.0, 1e-4, 0.0)
-@test t2 isa Table
-# These should both be equivalent
-@test t1 == t2
+
+# Test that we can interpolate across the range of valid Z
+for ZZ in range(extrema(Z(p))...; length=10)
+    @test isochrone(p, m, 10.0, ZZ, 0.0) isa Table
+end
+
+# Test that we can interpolate across the range of valid A_V
+# for ZZ in range(extrema(Z(p))...; length=10)
+#     @test_nowarn isochrone(p, m, 10.0, ZZ, 0.0)
+# end
