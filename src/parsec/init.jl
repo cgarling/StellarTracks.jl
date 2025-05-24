ff12da686af65d7da5e41f483fff20ded8167942
cgarling <chris.t.garling@gmail.com>
2025-05-24 16:59:54 -0400

    replace `split(path, "/")` with `splitpath(path)`
    Path separator is different on windows

--- src/parsec/init.jl
+++ src/parsec/init.jl
@@ -85,12 +85,15 @@ function custom_unpack(fname::AbstractString=joinpath(datadep"PARSECv1.2S", "rel
     @info "Reorganizing PARSEC v1.2S stellar tracks"
     # Process individual files into joined files
     @showprogress for dir in readdir(out_dir; join=true)
+        println("First data directory:\n")
+        dir |> println
         # Z, Y = dir_properties(dir)
         Z = parse(Float64, split(split(dir, 'Z')[end], 'Y')[1])
         Y = parse(Float64, split(dir, 'Y')[end])
-        dirstem = split(dir, "/")[end] # Something like Z0.0001Y0.249
+        # dirstem = split(dir, "/")[end] # Something like Z0.0001Y0.249
+        dirstem = splitpath(dir)[end] # Something like Z0.0001Y0.249
         dir = joinpath(dir, dirstem)
-        println("PARSEC data directory:\n")
+        println("Joined data directory:\n")
         dir |> println
         files = readdir(dir; join=true) # List of files in directory
         println("All files\n")
