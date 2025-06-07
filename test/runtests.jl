78cc893e9ede7be91c8a881a7e917fab8a7eeccc
cgarling <chris.t.garling@gmail.com>
2025-06-07 17:01:48 -0400

    Add BaSTIv1 datadep to doctestsetup

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,7 @@ using Test, SafeTestsets
 
 # Run doctests first
 using Documenter: DocMeta, doctest
-DocMeta.setdocmeta!(StellarTracks, :DocTestSetup, :(using StellarTracks; import BolometricCorrections; using DataDeps: @datadep_str; datadep"PARSECv1.2S"; datadep"MISTv1.2_vvcrit0.0"; datadep"MISTv1.2_vvcrit0.4"); recursive=true)
+DocMeta.setdocmeta!(StellarTracks, :DocTestSetup, :(using StellarTracks; import BolometricCorrections; using DataDeps: @datadep_str; datadep"PARSECv1.2S"; datadep"MISTv1.2_vvcrit0.0"; datadep"MISTv1.2_vvcrit0.4"; datadep"BaSTIv1"); recursive=true)
 doctest(StellarTracks)
 
 @testset verbose=true "StellarTracks.jl" begin
