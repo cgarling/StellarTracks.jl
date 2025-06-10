683b0ce22f6ec85f315ecd945f52d7cfce233873
cgarling <chris.t.garling@gmail.com>
2025-06-10 07:20:09 -0400

    stage bastiv2 datdep

--- src/basti/v2/init.jl
+++ src/basti/v2/init.jl
@@ -5,5 +5,6 @@ function __init__()
     register(DataDep("BaSTIv2",
                      """The updated BaSTI stellar models first presented in \
                         Hidalgo et al. 2018.""",
-                     "https://github.com/cgarling/StellarTracks.jl/releases/download/v0.0.2/basti_v2.jld2"))
+                     "https://github.com/cgarling/StellarTracks.jl/releases/download/v0.0.2/basti_v2.jld2",
+                     "b40eca2bb9ad97f0be48966085f2b2f8984ff4eb357e419af1d904ef0c9ac00d"))
 end
