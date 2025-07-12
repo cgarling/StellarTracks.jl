c4f4d4c66c8e24dc588b8cb8687301aec4911f52
cgarling <chris.t.garling@gmail.com>
2025-07-12 13:28:35 -0400

    Support ATLAS9 BC grid

--- src/StellarTracks.jl
+++ src/StellarTracks.jl
@@ -4,7 +4,7 @@ using ArgCheck: @argcheck
 using TypedTables: Table
 
 # For BCs.jl
-using BolometricCorrections: AbstractBCTable, MISTBCGrid, PHOENIXYBCGrid, filternames
+using BolometricCorrections: AbstractBCTable, AbstractBCGrid, MISTBCGrid, PHOENIXYBCGrid, ATLAS9YBCGrid, filternames
 import BolometricCorrections: AbstractChemicalMixture, X, X_phot, Y, Y_phot, Z, Z_phot, Y_p, MH, chemistry
 using DataInterpolations: PCHIPInterpolation
 import Tables
