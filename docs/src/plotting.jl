0023beba5720f2b91634f7e3d79db88130320d7a
cgarling <chris.t.garling@gmail.com>
2026-04-16 15:44:53 -0400

    Migrate docs from PyPlot.jl to CairoMakie.jl

--- docs/src/plotting.jl
+++ docs/src/plotting.jl
@@ -1,55 +1,42 @@
-import PyPlot as plt
-plt.ioff()
-ENV["MPLBACKEND"] = "agg"
-import PyPlot: @L_str # For LatexStrings
-plt.rc("text", usetex=true)
-plt.rc("font", family="serif", serif=["Computer Modern"], size=16)
-# This gets close but not quite
-# plt.matplotlib.rcParams["axes.formatter.use_mathtext"] = true
-# plt.rc("font", family="serif", serif=["cmr10"], size=14)
-plt.rc("figure", figsize=(5,5))
-plt.rc("patch", linewidth=1, edgecolor="k", force_edgecolor=true)
-# https://matplotlib.org/stable/gallery/images_contours_and_fields/interpolation_methods.html
-plt.rc("image", interpolation="none")
+using CairoMakie
+CairoMakie.activate!(type="svg")
 
 function plot_hr(iso)
-    fig,ax1 = plt.subplots() # hide
-    ax1.plot(iso.logTe, iso.Mbol) # hide
-    ax1.set_xlim([3.85, 3.5]) # hide
-    ax1.set_ylim(reverse(ax1.get_ylim())) # hide
-    ax1.set_xlabel("logTe") # hide
-    ax1.set_ylabel("Mbol") # hide
+    fig = Figure(size=(500, 500)) # hide
+    ax = Axis(fig[1,1], xlabel="logTe", ylabel="Mbol", # hide
+              xreversed=true, yreversed=true, # hide
+              limits=(3.5, 3.85, nothing, nothing)) # hide
+    lines!(ax, iso.logTe, iso.Mbol) # hide
     return fig # hide
 end
 
-function plot_cmd(iso; xfilters=(:F090W, :F150W), yfilter=:F090W, xlim=[0.4, 1.62])
-    fig,ax1 = plt.subplots() # hide
-    ax1.plot(getproperty(iso, xfilters[1]) .- getproperty(iso, xfilters[2]),
-             getproperty(iso, yfilter)) # hide
-    ax1.set_ylim(reverse(ax1.get_ylim())) # hide
-    ax1.set_xlim(xlim) # hide
-    ax1.set_xlabel(string(xfilters[1]) * " - " * string(xfilters[2])) # hide
-    ax1.set_ylabel(string(yfilter)) # hide
+function plot_cmd(iso; xfilters=(:F090W, :F150W), yfilter=:F090W, xlim=(0.4, 1.62))
+    colors = getproperty(iso, xfilters[1]) .- getproperty(iso, xfilters[2]) # hide
+    mags = getproperty(iso, yfilter) # hide
+    fig = Figure(size=(500, 500)) # hide
+    ax = Axis(fig[1,1], # hide
+              xlabel=string(xfilters[1]) * " - " * string(xfilters[2]), # hide
+              ylabel=string(yfilter), # hide
+              yreversed=true, # hide
+              limits=(xlim[1], xlim[2], nothing, nothing)) # hide
+    lines!(ax, colors, mags) # hide
     return fig # hide
 end
 
-# Plot mutiple isochrones on same HR diagram
+# Plot multiple isochrones on same HR diagram
 # p=BaSTIv1Library(0, true)
 # mh = -2.5:0.1:0.0
 # isos = [isochrone(p, 9, i) for i in mh]
 # plot_hrs(isos, mh)
 function plot_hrs(isos, mh)
-    fig,ax1 = plt.subplots()
-    cmap = plt.cm.gnuplot2
-    norm = plt.matplotlib.colors.Normalize(vmin=minimum(mh), vmax=maximum(mh))
+    fig = Figure(size=(600, 500))
+    ax = Axis(fig[1,1], xlabel="logTe", ylabel="Mbol",
+              xreversed=true, yreversed=true)
+    mh_min, mh_max = minimum(mh), maximum(mh)
     for (i, iso) in enumerate(isos)
-        ax1.plot(iso.logTe, iso.Mbol, c=cmap(norm(mh[i])))
+        t = (mh[i] - mh_min) / (mh_max - mh_min)
+        lines!(ax, iso.logTe, iso.Mbol, color=t, colormap=:gnuplot2, colorrange=(0, 1))
     end
-    # ax1.set_xlim([3.85, 3.5])
-    ax1.set_xlim(reverse(ax1.get_xlim()))
-    ax1.set_ylim(reverse(ax1.get_ylim()))
-    ax1.set_xlabel("logTe")
-    ax1.set_ylabel("Mbol")
-    fig.colorbar(plt.cm.ScalarMappable(cmap=cmap, norm=norm), ax=ax1)
+    Colorbar(fig[1,2], colormap=:gnuplot2, limits=(mh_min, mh_max))
     return fig
 end
